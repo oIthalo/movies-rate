2ecd1d64ee690bda5a5cb15efb2b25e144cd01b1
oIthalo <ithalobarreto333@gmail.com>
2025-01-15 04:39:45 -0300

    feat: flow to register user

---
+++ src/backend/MoviesRate.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using FluentValidation.Results;
+using MoviesRate.Communication.Requests;
+using MoviesRate.Communication.Response;
+using MoviesRate.Domain.Repositories;
+using MoviesRate.Domain.Repositories.User;
+using MoviesRate.Exception;
+using MoviesRate.Exception.Exceptions;
+
+namespace MoviesRate.Application.UseCases.User.Register;
+
+public class RegisterUserUseCase : IRegisterUserUseCase
+{
+    private readonly IReadUserRepository _readUserRepository;
+    private readonly IWriteUserRepository _writeUserRepository;
+    private readonly IMapper _mapper;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public RegisterUserUseCase(
+        IReadUserRepository readUserRepository, 
+        IWriteUserRepository writeUserRepository, 
+        IMapper mapper, 
+        IUnitOfWork unitOfWork)
+    {
+        _readUserRepository = readUserRepository;
+        _writeUserRepository = writeUserRepository;
+        _mapper = mapper;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<ShortUserResponse> Execute(RegisterUserRequest request)
+    {
+        await Validate(request);
+
+        var user = _mapper.Map<Domain.Entities.User>(request);
+
+        await _writeUserRepository.Add(user);
+        await _unitOfWork.Commit();
+
+        return new ShortUserResponse()
+        {
+            Name = user.Name,
+        };
+    }
+
+    private async Task Validate(RegisterUserRequest request)
+    {
+        var result = new RegisterUserValidator().Validate(request);
+
+        var existUser = await _readUserRepository.ExistActiveUserWithEmail(request.Email);
+        if (existUser) result.Errors.Add(new ValidationFailure(string.Empty, MessagesException.EMAIL_ALREADY_REGISTERED));
+
+        if (!result.IsValid) throw new ErrorOnValidationException(result.Errors.Select(x => x.ErrorMessage).ToList());
+    }
+}
---
+++ src/backend/MoviesRate.Application/UseCases/User/Register/RegisterUserValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using MoviesRate.Communication.Requests;
+using MoviesRate.Domain.ValueObjects;
+using MoviesRate.Exception;
+
+namespace MoviesRate.Application.UseCases.User.Register;
+
+public class RegisterUserValidator : AbstractValidator<RegisterUserRequest>
+{
+    public RegisterUserValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().WithMessage(MessagesException.NAME_EMPTY);
+        RuleFor(x => x.Email).NotEmpty().WithMessage(MessagesException.EMAIL_EMPTY);
+        RuleFor(x => x.Password).NotEmpty().WithMessage(MessagesException.PASSWORD_EMPTY);
+        When(x => !string.IsNullOrWhiteSpace(x.Email), () =>
+        {
+            RuleFor(x => x.Email).EmailAddress().WithMessage(MessagesException.EMAIL_INVALID);
+        });
+        When(x => !string.IsNullOrWhiteSpace(x.Password), () =>
+        {
+            RuleFor(x => x.Password.Length).GreaterThanOrEqualTo(MoviesRateRuleContracts.MIN_LENGTH_PASSWORD).WithMessage(MessagesException.PASSWORD_SHORT);
+        });
+    }
+}
