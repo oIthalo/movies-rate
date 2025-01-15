aeeed089ee21e86cf3b93bb856d1ee806a743748
oIthalo <ithalobarreto333@gmail.com>
2025-01-15 04:41:25 -0300

    feat: settings in program.cs

--- src/backend/MoviesRate.API/Program.cs
+++ src/backend/MoviesRate.API/Program.cs
@@ -1,3 +1,9 @@
+using MoviesRate.API.Filters;
+using MoviesRate.Application;
+using MoviesRate.Infrastructure;
+using MoviesRate.Infrastructure.Extensions;
+using MoviesRate.Infrastructure.Migrations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
+
+builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 
 var app = builder.Build();
 
@@ -17,9 +28,17 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
+MigrateDatabase();
+
 app.Run();
+
+void MigrateDatabase()
+{
+    var connectionStr = builder.Configuration.ConnectionString();
+    var serviceScope = app.Services.GetRequiredService<IServiceScopeFactory>().CreateScope();
+
+    DataBaseMigration.Migrate(connectionStr, serviceScope.ServiceProvider);
+}
