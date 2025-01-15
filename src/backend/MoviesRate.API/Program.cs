c7f3919505ed9894b4aac178c96cc31df7b2ef49
oIthalo <ithalobarreto333@gmail.com>
2025-01-15 04:42:44 -0300

    feat: settings to controller url stay in lower case

--- src/backend/MoviesRate.API/Program.cs
+++ src/backend/MoviesRate.API/Program.cs
@@ -18,6 +18,8 @@ builder.Services.AddApplication();
 
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
