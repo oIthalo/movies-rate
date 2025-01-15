53a72fef424c6c521e71bba0892c3473ea9e17bb
oIthalo <ithalobarreto333@gmail.com>
2025-01-15 04:40:41 -0300

    feat: adding projects to dependency injections

---
+++ src/backend/MoviesRate.Application/ApplicationDependencyInjection.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using MoviesRate.Application.Services.AutoMapper;
+using MoviesRate.Application.UseCases.User.Register;
+
+namespace MoviesRate.Application;
+
+public static class ApplicationDependencyInjection
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+        AddUseCases(services);
+        AddAutoMapper(services);
+    }
+
+    public static void AddUseCases(IServiceCollection services)
+    {
+        services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
+    }
+
+    public static void AddAutoMapper(IServiceCollection services)
+    {
+        services.AddScoped(option => new MapperConfiguration(autoMapperOptions =>
+        {
+            autoMapperOptions.AddProfile(new AutoMapping());
+        }).CreateMapper());
+    }
+}
