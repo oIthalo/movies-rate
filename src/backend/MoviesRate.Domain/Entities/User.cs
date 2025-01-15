3d5bb0637d145f0510773d031a12ecea8d8ad32b
oIthalo <ithalobarreto333@gmail.com>
2025-01-15 04:37:55 -0300

    feat: user repositories and entity

---
+++ src/backend/MoviesRate.Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿namespace MoviesRate.Domain.Entities;
+
+public class User : EntityBase
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public Guid Identifier { get; set; }
+}
