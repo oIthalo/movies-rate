6be0c791fabb2bceb05b4974453a6f0e5f21e751
oIthalo <ithalobarreto333@gmail.com>
2025-01-16 07:49:23 -0300

    feat: adding new exception and messages exceptions

---
+++ src/shared/MoviesRate.Exception/Exceptions/InvalidLoginException.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace MoviesRate.Exception.Exceptions;
+
+public class InvalidLoginException : MoviesRateException
+{
+    public InvalidLoginException() : base(MessagesException.INVALID_LOGIN)
+    {
+    }
+
+    public override IList<string> GetErrorMessages() => [Message];
+
+    public override HttpStatusCode GetStatusCode() => HttpStatusCode.BadRequest;
+}
