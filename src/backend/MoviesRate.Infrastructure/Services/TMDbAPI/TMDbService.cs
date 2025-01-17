2393ec57a21eecc1634cb6e38988b6e87230046a
oIthalo <ithalobarreto333@gmail.com>
2025-01-17 11:20:50 -0300

    feat: adding service to tmdb api and implementing the get 10 random movies top rated to dashboard

---
+++ src/backend/MoviesRate.Infrastructure/Services/TMDbAPI/TMDbService.cs
@@ -0,0 +1,25 @@
+﻿using MoviesRate.Domain.Entities;
+using MoviesRate.Domain.Interfaces;
+
+namespace MoviesRate.Infrastructure.Services.TMDbAPI;
+
+public class TMDbService : ITMDbService
+{
+    private readonly ITMDbApi _api;
+
+    public TMDbService(ITMDbApi api) =>  _api = api;
+
+    public async Task<MoviesList> Get10RandomTopRatedMovies()
+    {
+        var response = await _api.GetTopRated();
+        var randoms = response.Movies.OrderBy(x => Guid.NewGuid()).Take(10).ToList();
+
+        return new MoviesList()
+        {
+            Page = response.Page,
+            Movies = randoms ?? [],
+            TotalPages = response.TotalPages,
+            TotalResults = response.TotalResults,
+        };
+    }
+}
