5e98d527b5adc07fc4875617c9099d21e381ea65
oIthalo <ithalobarreto333@gmail.com>
2025-01-17 11:18:47 -0300

    feat: adding entities and dtos

---
+++ src/backend/MoviesRate.Domain/Dtos/MovieResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MoviesRate.Domain.Dtos;
+
+public class MovieResponseDto
+{
+    public int Id { get; set; }
+    public string? Overview { get; set; }
+    public string? PosterPath { get; set; }
+    public string? ReleaseDate { get; set; }
+    public string? Title { get; set; }
+}
---
+++ src/backend/MoviesRate.Domain/Entities/MoviesList.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MoviesRate.Domain.Entities;
+
+public class MoviesList
+{
+    [JsonPropertyName("page")]
+    public int Page { get; set; }
+
+    [JsonPropertyName("results")]
+    public List<Movie> Movies { get; set; } = [];
+
+    [JsonPropertyName("total_pages")]
+    public int TotalPages { get; set; }
+
+    [JsonPropertyName("total_results")]
+    public int TotalResults { get; set; }
+}
