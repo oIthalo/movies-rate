f5fe93f15def8468f2d20bdedbefb618e2fd5bed
oIthalo <ithalobarreto333@gmail.com>
2025-01-18 08:20:57 -0300

    feat: adding entity reviews

---
+++ src/backend/MoviesRate.Domain/Entities/Reviews.cs
@@ -0,0 +1,9 @@
+﻿namespace MoviesRate.Domain.Entities;
+
+public class Reviews : EntityBase
+{
+    public Guid UserIdentifier { get; set; }
+    public long MovieId { get; set; }
+    public string Comments { get; set; } = string.Empty;
+    public decimal Ratings { get; set; }
+}
