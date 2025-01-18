7422a8e746a45890b1a0295f3a34f9fdf3f352f7
oIthalo <ithalobarreto333@gmail.com>
2025-01-18 12:20:39 -0300

    feat: adding movie controller and method get movie by id

---
+++ src/backend/MoviesRate.API/Controllers/MoviesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesRate.Application.UseCases.Reviews.GetMovieById;
+using MoviesRate.Domain.Dtos;
+
+namespace MoviesRate.API.Controllers;
+
+public class MoviesController : MoviesRateControllerBase
+{
+    [HttpGet]
+    [Route("get-movie-by/{id}")]
+    [ProducesResponseType(typeof(MovieResponseDto), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetMovieById(
+        [FromServices] IGetMovieByIdUseCase useCase,
+        [FromRoute] int id)
+    {
+        var result = await useCase.Execute(id);
+        return Ok(result);
+    }
+}
