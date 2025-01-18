781bf37c0b2658aca8401c461c2dd7d4481c3a91
oIthalo <ithalobarreto333@gmail.com>
2025-01-18 12:18:32 -0300

    feat: adding entities to genres, and modifing other entities

---
+++ src/backend/MoviesRate.Domain/Entities/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MoviesRate.Domain.Entities;
+
+public class Genre
+{
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string? Name { get; set; }
+}
