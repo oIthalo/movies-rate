61446255f7b2f7d233728ba07943406789743070
oIthalo <ithalobarreto333@gmail.com>
2025-01-18 08:17:54 -0300

    feat: migration to table reviews

---
+++ src/backend/MoviesRate.Infrastructure/Migrations/Versions/Version0000002.cs
@@ -0,0 +1,16 @@
+﻿using FluentMigrator;
+
+namespace MoviesRate.Infrastructure.Migrations.Versions;
+
+[Migration(DataBaseVersions.TABLE_REVIEWS, "Table reviews to storage the movies reviews")]
+public class Version0000002 : VersionBase
+{
+    public override void Up()
+    {
+        CreateTable("Reviews")
+            .WithColumn("UserIdentifier").AsGuid().NotNullable().ForeignKey("FK_Reviews_User_Identifier", "Users", "Identifier")
+            .WithColumn("MovieId").AsInt64().NotNullable()
+            .WithColumn("Comments").AsString(500).NotNullable()
+            .WithColumn("Rating").AsDecimal().NotNullable();
+    }
+}
