7b71b67f4f3658b95964dccfc37924e686abc72c
oIthalo <ithalobarreto333@gmail.com>
2025-01-19 09:45:05 -0300

    feat: adding custom attribute is auth

---
+++ src/backend/MoviesRate.API/Attributes/IsAuthAttribute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesRate.API.Filters;
+
+namespace MoviesRate.API.Attributes;
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+public class IsAuthAttribute : TypeFilterAttribute
+{
+    public IsAuthAttribute() : base(typeof(IsAuthFilter))
+    {
+    }
+}
---
+++ src/backend/MoviesRate.API/Filters/IsAuthFilter.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using MoviesRate.Communication.Response;
+using MoviesRate.Domain.Security.Tokens.Validator;
+using MoviesRate.Exception;
+using MoviesRate.Domain.Repositories.User;
+using MoviesRate.Exception.Exceptions;
+
+namespace MoviesRate.API.Filters;
+
+public class IsAuthFilter : IAsyncAuthorizationFilter
+{
+    private readonly ITokenValidator _tokenValidator;
+    private readonly IReadUserRepository _userRepository;
+
+    public IsAuthFilter(
+        ITokenValidator tokenValidator,
+        IReadUserRepository userRepository)
+    {
+        _tokenValidator = tokenValidator;
+        _userRepository = userRepository;
+    }
+
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        try
+        {
+            var token = TokenOnRequest(context);
+
+            var userIdentifier = _tokenValidator.ValidateAndGetUserIdentifier(token);
+
+            var user = await _userRepository.GetUserByIdentifier(userIdentifier) ?? throw new UnauthorizedException(MessagesException.USER_WITHOUT_PERMISSION);
+        }
+        catch (SecurityTokenExpiredException)
+        {
+            context.Result = new UnauthorizedObjectResult(new ErrorResponse("Token is expired.")
+            {
+                TokenIsExpired = true,
+            });
+        }
+        catch (MoviesRateException moviesRateException)
+        {
+            context.HttpContext.Response.StatusCode = (int)moviesRateException.GetStatusCode();
+            context.Result = new ObjectResult(new ErrorResponse(moviesRateException.GetErrorMessages()));
+        }
+        catch
+        {
+            context.Result = new UnauthorizedObjectResult(new ErrorResponse(MessagesException.USER_WITHOUT_PERMISSION));
+        }
+    }
+
+    private static string TokenOnRequest(AuthorizationFilterContext context)
+    {
+        var authentication = context.HttpContext.Request.Headers.Authorization.ToString();
+        if (string.IsNullOrWhiteSpace(authentication))
+            throw new UnauthorizedException(MessagesException.NO_TOKEN);
+
+        return authentication["Bearer ".Length..].Trim();
+    }
+}
