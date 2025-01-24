9018279e55503034e56040d0dbed6c4324906b7c
oIthalo <ithalobarreto333@gmail.com>
2025-01-24 12:08:08 -0300

    feat: adding method to delete user on repository

--- src/backend/MoviesRate.Infrastructure/DataAccess/Repositories/User/WriteUserRepository.cs
+++ src/backend/MoviesRate.Infrastructure/DataAccess/Repositories/User/WriteUserRepository.cs
@@ -12,4 +12,6 @@ public class WriteUserRepository : IWriteUserRepository
     public async Task Add(Domain.Entities.User user) => await _dbContext.Users.AddAsync(user);
 
     public void Update(Domain.Entities.User user) => _dbContext.Users.Update(user);
+
+    public void Delete(Domain.Entities.User user) => _dbContext.Users.Remove(user);
 }
