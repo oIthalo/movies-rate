8f9a1505004f9fa89ce2e60485da4e363f2ee69e
oIthalo <ithalobarreto333@gmail.com>
2025-01-24 12:08:18 -0300

    feat: use case to delete user

--- src/backend/MoviesRate.Application/ApplicationDependencyInjection.cs
+++ src/backend/MoviesRate.Application/ApplicationDependencyInjection.cs
@@ -8,6 +8,7 @@ using MoviesRate.Application.UseCases.Dashboard.GetTopRated;
 using MoviesRate.Application.UseCases.Movies.GetMovieById;
 using MoviesRate.Application.UseCases.Movies.GetMoviesBySearch;
 using MoviesRate.Application.UseCases.Reviews.AddReview;
+using MoviesRate.Application.UseCases.User.Delete;
 using MoviesRate.Application.UseCases.User.Login;
 using MoviesRate.Application.UseCases.User.Register;
 using MoviesRate.Application.UseCases.User.Update;
@@ -27,6 +28,7 @@ public static class ApplicationDependencyInjection
         services.AddScoped<IRegisterUserUseCase, RegisterUserUseCase>();
         services.AddScoped<ILoginUserUseCase, LoginUserUseCase>();
         services.AddScoped<IUpdateUserUseCase, UpdateUserUseCase>();
+        services.AddScoped<IDeleteUserUseCase, DeleteUserUseCase>();
 
         services.AddScoped<IGetMoviesTopRatedUseCase, GetMoviesTopRatedUseCase>();
         services.AddScoped<IPopularMoviesDashboardUseCase, PopularMoviesDashboardUseCase>();
