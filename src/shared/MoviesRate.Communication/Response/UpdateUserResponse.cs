dc73991d5846a01ea6a40500c088c3465d372894
oIthalo <ithalobarreto333@gmail.com>
2025-01-24 10:04:52 -0300

    feat: communication to update user

---
+++ src/shared/MoviesRate.Communication/Response/UpdateUserResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace MoviesRate.Communication.Response;
+
+public class UpdateUserResponse
+{
+    public string Name { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
